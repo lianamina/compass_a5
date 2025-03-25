6e8b4f3a8e33458b498569d0470b33ceb7086747
Denise Ramos <ramosden0313@gmail.com>
2025-03-25 17:38:35 -0500

    other vc

---
+++ compass/viewitineraryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  viewitineraryViewController.swift
+//  compass
+//
+//  Created by Denise Ramos on 3/25/25.
+//
+
+import UIKit
+
+class viewitineraryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
