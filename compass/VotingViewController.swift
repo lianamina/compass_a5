fc4022611f79a6184747c54a43236c353388ae07
Denise Ramos <ramosden0313@gmail.com>
2025-03-25 17:35:55 -0500

    voting vc

---
+++ compass/VotingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  VotingViewController.swift
+//  compass
+//
+//  Created by Denise Ramos on 3/25/25.
+//
+
+import UIKit
+
+class VotingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
